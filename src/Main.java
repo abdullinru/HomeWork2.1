65341c34dad2337ae0fc5dc414ac1482ea71c3ee
abdullinru <abdullin17021989@gmail.com>
2022-06-08 17:26:55 +0600

    Задание 9,10

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,27 @@ public class Main {
 //        System.out.println(value);
         //Задание 6. Значение не меняется, так как при вызове метода создается копия переменной value.
         //далее value присваивается 22, но значение поля где хранится 33, никто не менял.
-        Integer[] value = {3, 4};
-        changeValue(value);
-        for (int i = 0; i < value.length; i++) {
-            System.out.println(value[i]);
-        }
+//        Integer[] value = {3, 4};
+//        changeValue(value);
+//        for (int i = 0; i < value.length; i++) {
+//            System.out.println(value[i]);
+//        }
         //задание 7. Снова значение не меняется. Так как значение переменных ссылочного типа
         // можно изменить только с помощью методов которые меняют значение полей.
         // Задание 8. Значение ячейки изменилось, так как в методе было обращение к памяти
         // где было записано 3, и туда записали значение 99.
+        Person person = new Person("Lyapis", "Trubetskoy");
+        changePerson(person);
+        System.out.println("person = " + person);
+        // Задание 9. Значения не меняются так как в методе создается новый объект класса Person,
+        // А исходный объект не изменился.
+        // Задание 10. Значения изменились, так как в методе мы непосредственно изменили значения полей объекта.
 
 
-
-
-
+    }
+    public static void changePerson(Person person) {
+        person.setName("Ilya");
+        person.setSurname("Lagutenko");
     }
 
     public static void changeValue(int value) {
