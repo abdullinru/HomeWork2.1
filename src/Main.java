c65b4338cda3295df8deeabc15af6ea6f7d0158d
abdullinru <abdullin17021989@gmail.com>
2022-06-08 16:35:53 +0600

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int value = 33;
+        changeValue(value);
+        System.out.println(value);
+        //Ответ будет 33, так значение переменной value не менялось.
+        //в методе changeValue была создана копия переменной и в нее записалось значение 22
+    }
+
+    public static void changeValue(int value) {
+        value = 22;
     }
 }
